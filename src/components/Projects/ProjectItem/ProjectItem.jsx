71ea705c0f913dcb6a377aa11cbd30b2638e7aa4
jakegodsall <jake.edward.godsall@gmail.com>
2023-06-05 16:43:57 +0100

    built ProjectItem component and rendered list from dummy object

--- src/components/Projects/ProjectItem/ProjectItem.jsx
+++ src/components/Projects/ProjectItem/ProjectItem.jsx
@@ -1,7 +1,32 @@
 import styles from './ProjectItem.module.scss';
 
-const ProjectItem = () => {
-    return <div>ProjectItem</div>;
+import Button from '../../UI/Button/Button';
+
+const ProjectItem = (props) => {
+    console.log(props.project);
+    return (
+        <div className={styles.projectItem}>
+            <div className={styles.projectItem_imageContainer}>
+                <img className={styles.projectItem_image} src={props.project.smallImage} />
+                <div className={styles.projectItem_imageOverlay}>
+                    <Button>View project</Button>
+                    <Button>View code</Button>
+                </div>
+            </div>
+            <div className={styles.projectItem_textContent}>
+                <h3 className={styles.projectItem_title}>{props.project.name}</h3>
+                <ul className={styles.projectItem_skillList}>
+                    {props.project.skills.map((skill, id) => {
+                        return (
+                            <li className={styles.projectItem_skillItem} key={id}>
+                                {skill}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default ProjectItem;
--- src/components/Projects/ProjectList/ProjectList.jsx
+++ src/components/Projects/ProjectList/ProjectList.jsx
@@ -3,6 +3,71 @@ import styles from './ProjectList.module.scss';
 import ProjectItem from '../ProjectItem/ProjectItem';
 import Button from '../../UI/Button/Button';
 
+import ProjectOneSmall from '../../../assets/images/thumbnail-project-1-small.webp';
+import ProjectTwoSmall from '../../../assets/images/thumbnail-project-2-small.webp';
+import ProjectThreeSmall from '../../../assets/images/thumbnail-project-3-small.webp';
+import ProjectFourSmall from '../../../assets/images/thumbnail-project-4-small.webp';
+import ProjectFiveSmall from '../../../assets/images/thumbnail-project-5-small.webp';
+import ProjectSixSmall from '../../../assets/images/thumbnail-project-6-small.webp';
+
+import ProjectOneLarge from '../../../assets/images/thumbnail-project-1-large.webp';
+import ProjectTwoLarge from '../../../assets/images/thumbnail-project-2-large.webp';
+import ProjectThreeLarge from '../../../assets/images/thumbnail-project-3-large.webp';
+import ProjectFourLarge from '../../../assets/images/thumbnail-project-4-large.webp';
+import ProjectFiveLarge from '../../../assets/images/thumbnail-project-5-large.webp';
+import ProjectSixLarge from '../../../assets/images/thumbnail-project-6-large.webp';
+
+const PROJECTS = [
+    {
+        name: 'design portfolio',
+        smallImage: ProjectOneSmall,
+        largeImage: ProjectOneLarge,
+        liveURL: '#/',
+        githubURL: '#/',
+        skills: ['html', 'css'],
+    },
+    {
+        name: 'e-learning landing page',
+        smallImage: ProjectTwoSmall,
+        largeImage: ProjectTwoLarge,
+        liveURL: '#/',
+        githubURL: '#/',
+        skills: ['html', 'css'],
+    },
+    {
+        name: 'todo web app',
+        smallImage: ProjectThreeSmall,
+        largeImage: ProjectThreeLarge,
+        liveURL: '#/',
+        githubURL: '#/',
+        skills: ['html', 'css', 'javascript'],
+    },
+    {
+        name: 'entertainment web app',
+        smallImage: ProjectFourSmall,
+        largeImage: ProjectFourLarge,
+        liveURL: '#/',
+        githubURL: '#/',
+        skills: ['html', 'css', 'javascript'],
+    },
+    {
+        name: 'memory game',
+        smallImage: ProjectFiveSmall,
+        largeImage: ProjectFiveLarge,
+        liveURL: '#/',
+        githubURL: '#/',
+        skills: ['html', 'css', 'javascript'],
+    },
+    {
+        name: 'art gallery showcase',
+        smallImage: ProjectSixSmall,
+        largeImage: ProjectSixLarge,
+        liveURL: '#/',
+        githubURL: '#/',
+        skills: ['html', 'css', 'javascript'],
+    },
+];
+
 const ProjectList = () => {
     return (
         <div className={styles.projectList}>
@@ -10,7 +75,11 @@ const ProjectList = () => {
                 <h2 className={styles.projectList_title}>Projects</h2>
                 <Button>Contact me</Button>
             </div>
-            <ProjectItem />
+            <div className={styles.projectList_list}>
+                {PROJECTS.map((project, id) => {
+                    return <ProjectItem project={project} key={id} />;
+                })}
+            </div>
         </div>
     );
 };
